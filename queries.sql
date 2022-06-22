ee3a176e2e4e5efa5bdb3aa514972482fbb6f45e
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-06-22 14:12:33 +0100

    fix: Make changes according to reviewer's request

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 
 SELECT * from animals WHERE name LIKE '%mon';
 SELECT name from animals WHERE date_of_birth BETWEEN 2016 AND 2019;
-SELECT name from animals WHERE neutered=True AND escape_attempts > 3;
+SELECT name from animals WHERE neutered=True AND escape_attempts < 3;
 SELECT date_of_birth from animals WHERE name="Agumon" or name="Pikachu";
 SELECT name,escape_attempts from animals WHERE weight_kg >10.5;
 SELECT * from animals  WHERE  neutered=TRUE;
